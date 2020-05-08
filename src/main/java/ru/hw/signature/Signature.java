5f41f36a6f8b2c15fde86d27147951c22633b29c
nikta <n.i.k.tver@yandex.ru>
2020-05-08 14:58:41 +0300

    own hash

--- src/main/java/ru/hw/signature/Signature.java
+++ src/main/java/ru/hw/signature/Signature.java
@@ -8,21 +8,27 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import java.math.BigInteger;
 import java.util.Arrays;
 
-import static ru.hw.signature.Utils.*;
+import static ru.hw.signature.Utils.unsignedInt;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Signature {
+    public static boolean useStandardHashCode = true;
 
     public static boolean verify(byte[] data, byte[] sign, ImmutablePair<BigInteger, BigInteger> privateKey) {
-        val hash = BigInteger.valueOf(unsignedInt(Arrays.hashCode(data))).toByteArray();
+        val hash = hash(data);
         val deciphered = RSA.decipher(sign, privateKey.left, privateKey.right);
 
         return Arrays.equals(hash, deciphered);
     }
 
     public static byte[] sign(byte[] data, ImmutablePair<BigInteger, BigInteger> openKey) {
-        val hash = BigInteger.valueOf(unsignedInt(Arrays.hashCode(data))).toByteArray();
-
+        val hash = hash(data);
         return RSA.encipher(hash, openKey.left, openKey.right);
+    }
+
+    private static byte[] hash(byte[] data) {
+        return useStandardHashCode ?
+                BigInteger.valueOf(unsignedInt(Arrays.hashCode(data))).toByteArray() :
+                new BigInteger(data).toByteArray();
     }
 }
--- src/test/java/ru/hw/signature/TestSign.java
+++ src/test/java/ru/hw/signature/TestSign.java
@@ -5,6 +5,10 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.RepeatedTest;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class TestSign {
     @RepeatedTest(20)
     public void testRSAWithGeneratedPAndQ() {
@@ -28,5 +32,21 @@ public class TestSign {
         val signedData = Signature.sign(text, openPair);
 
         Assertions.assertTrue(Signature.verify(text, signedData, privatePair));
+    }
+
+    @RepeatedTest(20)
+    public void test() throws IOException {
+        var input = Files.readAllBytes(Paths.get("C:\\Users\\niktv\\Downloads\\text.txt"));
+
+        val rsa = new RSA();
+
+        val openPair = rsa.generatePublicKey();
+        val privatePair = rsa.generatePrivateKey();
+
+        Signature.useStandardHashCode = false;
+
+        val data = Signature.sign(input, openPair);
+
+        Assertions.assertTrue(Signature.verify(input, data, privatePair));
     }
 }
